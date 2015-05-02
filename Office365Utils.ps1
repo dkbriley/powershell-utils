4e174a9f66436b780abf70d8b044fdb50ac642db
dbriley <daniel@briley.org.uk>
2015-05-02 22:58:24 +0100

    Added initial version of Office365Utils.ps1

---
+++ Office365Utils.ps1
@@ -0,0 +1,11 @@
+﻿
+function Connect-ExchangeOnline
+{
+    $Credentials = Get-Credential -Message 'Enter your Exchange Online credentials'
+
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange `
+        -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+        -Credential $Credentials -Authentication Basic -AllowRedirection
+
+    Import-PSSession $Session
+}
