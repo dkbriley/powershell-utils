ae2c6853ed099f064b99fddb8e9dc658ba5ac0b6
dbriley <daniel@briley.org.uk>
2015-05-03 13:46:53 +0100

    Initial version of Set-MicrosoftUpdate.ps1

---
+++ Set-MicrosoftUpdate.ps1
@@ -0,0 +1,18 @@
+﻿function Set-MicrosoftUpdate
+{
+    param([bool]$Enabled)
+
+    $ServiceManager = New-Object -ComObject Microsoft.Update.ServiceManager
+    $ServiceManager.ClientApplicationID = 'Set-MicrosoftUpdate'
+
+    if ($Enabled)
+    {
+        # Add the Microsoft Update service GUID 
+        $ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
+    }
+    else
+    {
+        # Remove the Microsoft Update service GUID 
+        $ServiceManager.RemoveService("7971f918-a847-4430-9279-4a52d1efe18d")
+    }
+}
