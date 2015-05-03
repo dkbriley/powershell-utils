551f66ae3986924e2efeb4a216bdc2d611c0fa86
dbriley <daniel@briley.org.uk>
2015-05-03 13:38:53 +0100

    Initial version of Test-RunningAsAdministrator.ps1

---
+++ Test-RunningAsAdministrator.ps1
@@ -0,0 +1,8 @@
+﻿function Test-RunningAsAdministrator
+{
+    $Principal = New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent())
+
+    $Principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
