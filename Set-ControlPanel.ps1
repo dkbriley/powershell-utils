ddf63c1e902aa1c83f301de1c3c739ab61abdcfb
dbriley <daniel@briley.org.uk>
2015-05-03 22:39:31 +0100

    Added initial version of Set-ControlPanel

---
+++ Set-ControlPanel.ps1
@@ -0,0 +1,36 @@
+﻿function Set-ControlPanel
+{
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("Category", "SmallIcon", "LargeIcon")]
+        [string]$ViewMode
+    )
+
+    $RegKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel'
+
+    switch ($ViewMode)
+    {
+        "Category"
+        {
+            # Set the startup page to 'category' view (as opposed to 'icon' view)
+            Set-ItemProperty $RegKey -Name StartupPage -Value 0 -Type DWord
+        }
+
+        "SmallIcon"
+        {
+            # Set the startup page to 'icon' view (as opposed to 'category' view)
+            Set-ItemProperty $RegKey -Name StartupPage -Value 1 -Type DWord
+            # Set the mode of the icon view to small icons
+            Set-ItemProperty $RegKey -Name AllItemsIconView -Value 1 -Type DWord
+        }
+
+        "LargeIcon"
+        {
+            # Set the startup page to 'icon' view (as opposed to 'category' view)
+            Set-ItemProperty $RegKey -Name StartupPage -Value 1 -Type DWord
+            # Set the mode of the icon view to large icons
+            Set-ItemProperty $RegKey -Name AllItemsIconView -Value 0 -Type DWord
+        }
+    }
+}
